2e74ee4b53a5a33c533f616cd490b78c4f7df81b
aqyra <varreneaqyra@gmail.com>
2023-03-25 17:41:19 +0800

    react Navbar

---
+++ scrm/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+
+const App = () => {
+    return (
+        <>
+            <Navbar/>
+            
+            
+        </>
+    )
+}
+
+export default App
