d90d08d1ef984f53f606e710e667014e51e65d29
aqyra <varreneaqyra@gmail.com>
2023-03-27 22:10:11 +0800

    horizontal card fixed

--- scrm/src/Pages/Wishlist/Wishlist.jsx
+++ scrm/src/Pages/Wishlist/Wishlist.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import "./wishlist.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import HorizontalCard from '../../Components/Card/HorizontalCard';
+import img from '../../Assets/img.jpg'
+import {MdOutlineEdit} from 'react-icons/md'
 
 const Wishlist = () => {
     return (
@@ -10,15 +12,31 @@ const Wishlist = () => {
                 <h1>Wishlist</h1>
             </div>
             
-            <div className="firstPart">
-                <h2>Trips You Love</h2>
-
-                <div className="list">
-                    <HorizontalCard />
-                    <HorizontalCard />
-                    <HorizontalCard />
-                    
-                </div> 
+            <div className="sec">
+                <div className="secTitle">
+                    <h2>Trips You Love</h2>
+                </div>
+                <div className="subSec">
+                    <div className="subSecTitle">
+                        <h3>Summer 2023</h3><button className="btn-no"><MdOutlineEdit className="icon"/></button>
+                    </div>
+                    <div className="list">
+                        <HorizontalCard />
+                        <HorizontalCard />
+                        <HorizontalCard />
+                    </div> 
+                </div>
+                <div className="subSec">
+                    <div className="subSecTitle">
+                        <h3>USA</h3><button className="btn-no"><MdOutlineEdit className="icon"/></button>
+                    </div>
+                    <div className="list">
+                        <HorizontalCard />
+                        <HorizontalCard />
+                        <HorizontalCard />
+                    </div> 
+                </div>
+                
             </div>
             
         </>
