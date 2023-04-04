c2abc95b95f25c660dac67e66ca79010cc469fa8
Natalienovaela <natasharafaelaw@gmail.com>
2023-04-04 20:59:47 +0800

    datainit

--- KiaKia-ejb/src/java/datainit/DataInitSessionBean.java
+++ KiaKia-ejb/src/java/datainit/DataInitSessionBean.java
@@ -5,8 +5,11 @@
  */
 package datainit;
 
+import entity.Note;
 import entity.Trip;
 import entity.User;
+import error.TripNotFoundException;
+import error.UnknownPersistenceException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import javax.annotation.PostConstruct;
@@ -16,6 +19,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import session.NoteSessionBeanLocal;
 import session.UserSessionBeanLocal;
 
 /**
@@ -26,6 +30,9 @@ import session.UserSessionBeanLocal;
 @LocalBean
 @Startup
 public class DataInitSessionBean {
+
+    @EJB(name = "NoteSessionBeanLocal")
+    private NoteSessionBeanLocal noteSessionBeanLocal;
 
     @PersistenceContext(unitName = "KiaKia-ejbPU")
     private EntityManager em;
@@ -50,11 +57,26 @@ public class DataInitSessionBean {
     }
     
     public void initialiseTrip() {
+        try {
         Trip trip = new Trip("First Trip", new GregorianCalendar(2024, Calendar.FEBRUARY, 11).getTime(), new GregorianCalendar(2024, Calendar.FEBRUARY, 15).getTime());
         em.persist(trip);
+            em.flush();
+            Note note1 = new Note("My First Note", "bla", false);
+            noteSessionBeanLocal.createNewNote(note1, trip.getTripId());
+            Note note2 = new Note("My Second Note", "blabla", false);
+            noteSessionBeanLocal.createNewNote(note2, trip.getTripId());
         
         Trip trip2 = new Trip("Second Trip", new GregorianCalendar(2024, Calendar.JUNE, 15).getTime(), new GregorianCalendar(2024, Calendar.JUNE, 28).getTime());
         em.persist(trip2);
+            em.flush();
+            Note note3 = new Note("My Third Note", "blablabla", false);
+            noteSessionBeanLocal.createNewNote(note3, trip2.getTripId());
+
+        } catch (UnknownPersistenceException ex) {
+            Logger.getLogger(DataInitSessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TripNotFoundException ex) {
+            Logger.getLogger(DataInitSessionBean.class.getName()).log(Level.SEVERE, null, ex);
+    }
     }
     
 }
