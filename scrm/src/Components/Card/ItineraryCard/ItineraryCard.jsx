13672b5760a9c3520442c69a3af79cb3cb6c78f0
aqyra <varreneaqyra@gmail.com>
2023-04-09 14:22:38 +0800

    login and signup styling changes

--- scrm/src/Components/Card/ItineraryCard/ItineraryCard.jsx
+++ scrm/src/Components/Card/ItineraryCard/ItineraryCard.jsx
@@ -1,77 +1,69 @@
-import React, { useState } from 'react'
-import './itinerarycard.css'
-import image from '../../../Assets/img.jpg'
-import Chip from '@mui/material/Chip'
-import Profile from '../../Profile/Profile'
-import Icon from '@mui/material/Icon'
-import IconButton from '@mui/material/IconButton'
-import {FaRegBookmark, FaBookmark} from 'react-icons/fa'
+import React, { useState } from "react";
+import "./itinerarycard.css";
+import image from "../../../Assets/img.jpg";
+import Chip from "@mui/material/Chip";
+import Profile from "../../Profile/Profile";
+import Icon from "@mui/material/Icon";
+import IconButton from "@mui/material/IconButton";
+import { FaRegBookmark, FaBookmark } from "react-icons/fa";
 
 const dummyData = [
   {
     img: image,
     places: ["Marina Bay Sands", "Haw Par Villa", "Lau Pa Sat"],
-    tags: ["Singapore", "SEA", "Summer"]
-  }
-  
-]
+    tags: ["Singapore", "SEA", "Summer"],
+  },
+];
 
 function BookmarkButton() {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
-    setIsHovered(true)
-  }
+    setIsHovered(true);
+  };
 
   const handleMouseLeave = () => {
-    setIsHovered(false)
-  }
+    setIsHovered(false);
+  };
 
   return (
-    <IconButton 
+    <IconButton
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="bookmark-button">
-        {isHovered ? <FaRegBookmark /> : <FaBookmark/>}
+      className="bookmark-button"
+    >
+      {isHovered ? <FaRegBookmark /> : <FaBookmark />}
     </IconButton>
-  )
+  );
 }
 
 const ItineraryCard = (props) => {
-  const placeHighlight = props.places?.map((place) => <li className="place">{place}</li>);
-  
-  const tags = props.tags?.map(((tag) => <Chip label={tag}></Chip> ))
+  const placeHighlight = props.places?.map((place) => (
+    <li className="place">{place}</li>
+  ));
 
- 
+  const tags = props.tags?.map((tag) => <Chip label={tag}></Chip>);
+
   return (
     <div className="itinerary-card">
-
       <div className="itinerary-imageDiv">
-        
-        <img src={props.img} alt="City" className='itinerary-card-image'/>
-    
-            <Profile />
-            <BookmarkButton/>
-          
+        <img src={props.img} alt="City" className="itinerary-card-image" />
+
+        <Profile />
+        <BookmarkButton />
         <div className="city-div">
           <h2>{props.cityName}</h2>
-          <p className='num-of-days'>{props.numOfDays} days</p>
+          <p className="num-of-days">{props.numOfDays} days</p>
         </div>
       </div>
 
       <div className="card-details">
-        <ul className='place-highlight'>
-          {placeHighlight}
-        </ul>
-        <p className='card-desc'>{props.desc}</p>
-        <div className="tags">
-            {tags}
-        </div>
+        <ul className="place-highlight">{placeHighlight}</ul>
+        <p className="card-desc">{props.desc}</p>
+        <div className="tags">{tags}</div>
       </div>
-      
     </div>
-    
-  )
-}
+  );
+};
 
-export default ItineraryCard
+export default ItineraryCard;
--- scrm/src/Pages/Signup/Signup.jsx
+++ scrm/src/Pages/Signup/Signup.jsx
@@ -1,174 +1,233 @@
-import React, { useState } from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { useNavigate, Link } from 'react-router-dom';
-import './Signup.css';
-import Api from '../../Helpers/Api';
+import { useNavigate, Link } from "react-router-dom";
+import "./Signup.css";
+import Api from "../../Helpers/Api";
 
 function Signup({ handleLogin }) {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [errors, setErrors] = useState({});
-    const [showPassword, setShowPassword] = React.useState(false);
-    const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = React.useState(false);
+  const navigate = useNavigate();
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-    const handleNameChange = (event) => {
-        const nameValue = event.target.value;
-        setName(nameValue);
-        setErrors((prevErrors) => ({
-            ...prevErrors,
-            name: nameValue.trim().length === 0 ? "Name cannot be empty!" : undefined
-        }));
-    };
+  const handleNameChange = (event) => {
+    const nameValue = event.target.value;
+    setName(nameValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      name: nameValue.trim().length === 0 ? "Name cannot be empty!" : undefined,
+    }));
+  };
 
-    const handleEmailChange = (event) => {
-        const emailValue = event.target.value;
-        setEmail(emailValue);
-        setErrors((prevErrors) => ({
-            ...prevErrors,
-            email: !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue) ? "Email must be a valid email address!" : undefined
-        }));
-    };
+  const handleEmailChange = (event) => {
+    const emailValue = event.target.value;
+    setEmail(emailValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      email: !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)
+        ? "Email must be a valid email address!"
+        : undefined,
+    }));
+  };
 
-    const handlePasswordChange = (event) => {
-        const passwordValue = event.target.value;
-        setPassword(passwordValue);
-        setErrors((prevErrors) => ({
-            ...prevErrors,
-            password: passwordValue.length < 8 || !/[a-z]/.test(passwordValue) || !/[A-Z]/.test(passwordValue) || !/[0-9]/.test(passwordValue) ? "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number!" : undefined,
-            confirmPassword: confirmPassword !== passwordValue ? "Passwords do not match!" : undefined
-        }));
-    };
+  const handlePasswordChange = (event) => {
+    const passwordValue = event.target.value;
+    setPassword(passwordValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      password:
+        passwordValue.length < 8 ||
+        !/[a-z]/.test(passwordValue) ||
+        !/[A-Z]/.test(passwordValue) ||
+        !/[0-9]/.test(passwordValue)
+          ? "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number!"
+          : undefined,
+      confirmPassword:
+        confirmPassword !== passwordValue
+          ? "Passwords do not match!"
+          : undefined,
+    }));
+  };
 
-    const handleConfirmPasswordChange = (event) => {
-        const confirmPasswordValue = event.target.value;
-        setConfirmPassword(confirmPasswordValue);
-        setErrors((prevErrors) => ({
-            ...prevErrors,
-            confirmPassword: confirmPasswordValue !== password ? "Passwords do not match!" : undefined
-        }));
-    };
+  const handleConfirmPasswordChange = (event) => {
+    const confirmPasswordValue = event.target.value;
+    setConfirmPassword(confirmPasswordValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      confirmPassword:
+        confirmPasswordValue !== password
+          ? "Passwords do not match!"
+          : undefined,
+    }));
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formErrors = {};
-        if (name.trim().length === 0) {
-            formErrors.name = "Name cannot be empty!";
-        }
-        if (!/^[^\s@]+@[^\s@]+.[^\s@]+$/.test(email)) {
-            formErrors.email = "Email must be a valid email address!";
-        }
-        if (password.length < 8 || !/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/[0-9]/.test(password)) {
-            formErrors.password = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number!";
-        }
-        if (confirmPassword !== password) {
-            formErrors.confirmPassword = "Passwords do not match!";
-        }
-        if (Object.keys(formErrors).length > 0) {
-            setErrors(formErrors);
-        } else {
-            Api.createUser({ name, email, password })
-                .then(response => response.json())
-                .then(data => {
-                    const userId = data.userId;
-                    navigate(`/Home/${userId}`);
-                    handleLogin(true);
-                })
-                .catch((error) => {
-                    console.log(email + " " + password);
-                    setErrors({ submit: error.message });
-                });
-        }
-    };
-    return (
-        <Box className="signup-container" display="flex" flexDirection={"column"} maxWidth={500} justifyContent={"center"} margin="auto" marginTop={10} padding={3} borderRadius={"5px"} boxShadow={"2px 2px 15px"}>
-            <Typography fontWeight='bold' variant="h4" textAlign="center" marginTop={7}>Start KiaKia</Typography>
-            <Typography textAlign="center" marginBottom={5}>Create new account</Typography>
-            <form className="signup-form" onSubmit={handleSubmit}>
-                <Typography align="left">Name</Typography>
-                <TextField
-                    fullWidth
-                    variant="outlined"
-                    margin="dense"
-                    value={name}
-                    placeholder='Enter full name'
-                    onChange={handleNameChange}
-                    error={Boolean(errors.name)}
-                    helperText={errors.name}
-                />
-                <Typography align="left" marginTop={2}>Email</Typography>
-                <TextField
-                    fullWidth
-                    variant="outlined"
-                    margin="dense"
-                    value={email}
-                    placeholder='Enter email address'
-                    onChange={handleEmailChange}
-                    error={Boolean(errors.email)}
-                    helperText={errors.email}
-                />
-                <Typography align="left" marginTop={2}>Password</Typography>
-                <TextField
-                    fullWidth
-                    variant="outlined"
-                    margin="dense"
-                    type={showPassword ? "text" : "password"}
-                    value={password}
-                    placeholder='Enter password'
-                    onChange={handlePasswordChange}
-                    error={Boolean(errors.password)}
-                    helperText={errors.password}
-                    InputProps={{
-                        endAdornment: (
-                            <Button
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </Button>
-                        )
-                    }}
-                />
-                <Typography align="left" marginTop={2}>Confirm Password</Typography>
-                <TextField
-                    fullWidth
-                    variant="outlined"
-                    margin="dense"
-                    type={showPassword ? "text" : "password"}
-                    value={confirmPassword}
-                    placeholder='Enter same password'
-                    onChange={handleConfirmPasswordChange}
-                    error={Boolean(errors.confirmPassword)}
-                    helperText={errors.confirmPassword}
-                />
-                {errors.submit && (
-                    <Typography color="error" className="signup-error">{errors.submit}</Typography>
-                )}
-                <Button
-                    variant="contained"
-                    color="warning"
-                    type="submit"
-                    sx={{ marginTop: 3 }}
-                    className="signup-submit-button"
-                    fullWidth
-                >
-                    Create new account
-                </Button>
-                <Typography align="left" marginTop={2} style={{ display: 'inline-block' }}>Have an existing account?{' '}
-                    <Link to="/Login" className="logo flex" style={{ display: 'inline-block' }}> Log in</Link>
-                </Typography>
-            </form>
-        </Box>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formErrors = {};
+    if (name.trim().length === 0) {
+      formErrors.name = "Name cannot be empty!";
+    }
+    if (!/^[^\s@]+@[^\s@]+.[^\s@]+$/.test(email)) {
+      formErrors.email = "Email must be a valid email address!";
+    }
+    if (
+      password.length < 8 ||
+      !/[a-z]/.test(password) ||
+      !/[A-Z]/.test(password) ||
+      !/[0-9]/.test(password)
+    ) {
+      formErrors.password =
+        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number!";
+    }
+    if (confirmPassword !== password) {
+      formErrors.confirmPassword = "Passwords do not match!";
+    }
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+      Api.createUser({ name, email, password })
+        .then((response) => response.json())
+        .then((data) => {
+          const userId = data.userId;
+          navigate(`/Home/${userId}`);
+          handleLogin(true);
+        })
+        .catch((error) => {
+          console.log(email + " " + password);
+          setErrors({ submit: error.message });
+        });
+    }
+  };
+  return (
+    <Box
+      className="signup-container"
+      display="flex"
+      flexDirection={"column"}
+      maxWidth={500}
+      justifyContent={"center"}
+      margin="auto"
+      marginTop={10}
+      padding={3}
+      borderRadius={"5px"}
+      boxShadow={"2px 2px 15px #ccc"}
+    >
+      <Typography
+        fontWeight="bold"
+        variant="h4"
+        textAlign="center"
+        marginTop={7}
+      >
+        Start KiaKia
+      </Typography>
+      <Typography textAlign="center" marginBottom={5}>
+        Create new account
+      </Typography>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <Typography align="left">Name</Typography>
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="dense"
+          value={name}
+          placeholder="Enter full name"
+          onChange={handleNameChange}
+          error={Boolean(errors.name)}
+          helperText={errors.name}
+        />
+        <Typography align="left" marginTop={2}>
+          Email
+        </Typography>
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="dense"
+          value={email}
+          placeholder="Enter email address"
+          onChange={handleEmailChange}
+          error={Boolean(errors.email)}
+          helperText={errors.email}
+        />
+        <Typography align="left" marginTop={2}>
+          Password
+        </Typography>
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="dense"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          placeholder="Enter password"
+          onChange={handlePasswordChange}
+          error={Boolean(errors.password)}
+          helperText={errors.password}
+          InputProps={{
+            endAdornment: (
+              <Button
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </Button>
+            ),
+          }}
+        />
+        <Typography align="left" marginTop={2}>
+          Confirm Password
+        </Typography>
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="dense"
+          type={showPassword ? "text" : "password"}
+          value={confirmPassword}
+          placeholder="Enter same password"
+          onChange={handleConfirmPasswordChange}
+          error={Boolean(errors.confirmPassword)}
+          helperText={errors.confirmPassword}
+        />
+        {errors.submit && (
+          <Typography color="error" className="signup-error">
+            {errors.submit}
+          </Typography>
+        )}
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ marginTop: 3 }}
+          className="signup-submit-button"
+          fullWidth
+        >
+          Create new account
+        </Button>
+        <Typography
+          align="left"
+          marginTop={2}
+          style={{ display: "inline-block" }}
+        >
+          Have an existing account?{" "}
+          <Link
+            to="/Login"
+            className="action-link flex"
+            style={{ display: "inline-block" }}
+          >
+            {" "}
+            Log in
+          </Link>
+        </Typography>
+      </form>
+    </Box>
+  );
 }
 export default Signup;
