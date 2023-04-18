8ab3a14c4832aceabe052105c9c45c65e9240059
aqyra <69248412+aqyra@users.noreply.github.com>
2023-04-18 11:35:27 +0800

    places component

--- scrm/src/Pages/Explore/Places/Places.jsx
+++ scrm/src/Pages/Explore/Places/Places.jsx
@@ -8,39 +8,68 @@ import Emoji from "a11y-react-emoji";
 import Api from "../../../Helpers/Api";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import SavePlaceToWishlist from "./SavePlaceToWishlist";
 
-const dummyData = [
-  {
-    city: "Singapore",
-    country: "Singapore",
-    img: singapore,
-  },
-  {
-    city: "Tokyo",
-    country: "Japan",
-    img: tokyo,
-  },
-];
-const Places = () => {
+const Places = ({ userId }) => {
   const [inputValue, setInputValue] = React.useState("");
-  const placeCards = dummyData?.map((cardData) => (
-    <PlaceCard key={cardData.id} {...cardData} />
-  ));
-
+  const [thisUserId, setUserId] = useState("");
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
   const [places, setPlaces] = useState([]);
+  const [placesData, setPlacesData] = useState([]);
 
-  // to get country list for search bar value
+  const handleCloseButton = () => {
+    console.log(selectedCard);
+    setShowPopup(false);
+    setSelectedCard(false);
+  };
+
+  const handleCardClick = (card) => {
+    setShowPopup(true);
+    setSelectedCard(card);
+    console.log("this place is clicked");
+  };
   useEffect(() => {
-    Api.getCountryList()
+    setUserId(userId);
+  }, [userId]);
+
+  const handleSaveButton = async () => {
+    if (!selectedCard) {
+      alert("No card has been selected");
+      return;
+    }
+
+    try {
+      await Api.linkUserWithWishlistPlace(userId, selectedCard.placeId);
+      setShowPopup(false);
+      setSelectedCard(null);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  //const placeCards = dummyData?.map((cardData) => (
+  //<PlaceCard key={cardData.id} {...cardData} />
+  //));
+
+  const getPlaces = () => {
+    Api.getAllPlaces()
       .then((response) => response.json())
       .then((data) => {
-        setPlaces(data);
-      })
-      .catch((error) => {
-        console.log("Error while retrieving country list");
+        setPlacesData(data);
       });
+  };
+
+  useEffect(() => {
+    getPlaces();
   }, []);
-  const [value, setValue] = React.useState(places[0]);
+  const placeCards = placesData?.map((data) => (
+    <PlaceCard
+      key={data.id}
+      {...data}
+      onClick={() => handleCardClick(data)}
+      userId={userId}
+    />
+  ));
 
   return (
     <>
@@ -48,24 +77,15 @@ const Places = () => {
         <p className="page-content">Find your next destination</p>
         <Emoji symbol="📍" label="earth emoji" className="icon" />
       </div>
-      <Autocomplete
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={places}
-        sx={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Search places" />
-        )}
-      />
 
       <div className="cards">{placeCards}</div>
+      {selectedCard && (
+        <SavePlaceToWishlist
+          selectedCard={selectedCard}
+          onCloseButtonClick={handleCloseButton}
+          onSaveButtonClick={handleSaveButton}
+        />
+      )}
     </>
   );
 };
