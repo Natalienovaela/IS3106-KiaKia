8463f0b7c7b7c8b54cc6e470df2170d33f1d2994
aqyra <69248412+aqyra@users.noreply.github.com>
2023-04-18 23:01:18 +0800

    api fixed

--- scrm/src/Helpers/Api.js
+++ scrm/src/Helpers/Api.js
@@ -211,7 +211,7 @@ const Api = {
     },
 
     removePlaceLineItem(tripId, itineraryId, placeLineItemId) {
-        return fetch(`${SERVER_PREFIX}/trips/${tripId}/itineraries/${itineraryId}/placeLineItems/${placeLineItemId}`, {
+        return fetch(`${SERVER_PREFIX}/trips/${tripId}/itineraries/${itineraryId}/placeLineItems/${placeLineItemId}/remove`, {
             method: "DELETE",
         })
     },
